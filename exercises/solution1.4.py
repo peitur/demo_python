91be991985176a4ef97b0185787eba486e1d21dd
peitur <peitur@gmail.com>
2020-05-12 09:41:11 +0200

    initisl finc

---
+++ exercises/solution1.4.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def instance( obj, t ):
+    if type( obj ).__name__ == t:
+        return True
+    return False
+
+def dict_decode( data ):
+    pass
+
+if __name__ == "__main__":
+    pass
