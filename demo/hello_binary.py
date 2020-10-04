c2dec83bd5def90ad33cf799b844dfac74ac9528
peitur <peitur@gmail.com>
2020-10-04 19:35:56 +0200

    started bin stuff

--- demo/hello_binary.py
+++ demo/hello_binary.py
@@ -15,6 +15,25 @@ def dec( msg, k ):
     b = format( msg ^ k, 'x' )
     return binascii.unhexlify( ( '0' * (len( b ) % 2 )) + b )
 
+def file_n_bytes( filename, n ):
+    try:
+        line = ""
+        with open( filename, "rb" ) as fd:
+            byte = fd.read(1)
+            i = 0
+            while i < n:
+                print( "%s " % ( str( binascii.hexlify( byte ) ) ), end="" )
+                byte  = fd.read(1)
+                i += 1
+
+                if i == n:
+                    print()
+
+    except Exception as e:
+        pprint( e )
+
+
+
 if __name__ == "__main__":
 
     x = xorStr( "tets1tets1tets1tets1tets1tets1tets1tets1" )
@@ -24,3 +43,5 @@ if __name__ == "__main__":
     pprint( x )
     pprint( e )
     pprint( d )
+
+    file_n_bytes( "hello_binary.py", 10)
