410b0cce2018e61d2d8273bc1e501e7e457690ba
peitur <peitur@gmail.com>
2021-01-19 12:32:44 +0100

    wip, first part of mapreduce experiment

--- basic/multiproc.py
+++ basic/multiproc.py
@@ -7,6 +7,123 @@ import time, random
 
 from pprint import pprint
 
+## REF: https://pymotw.com/2/multiprocessing/communication.html
+
+## ----------------------------------------------------------
+## Events
+## ----------------------------------------------------------
+
+
+## ----------------------------------------------------------
+## Pool
+## ----------------------------------------------------------
+
+
+## ----------------------------------------------------------
+## Shared namespaces
+## ----------------------------------------------------------
+
+
+## ----------------------------------------------------------
+## Shared states
+## ----------------------------------------------------------
+
+
+## ----------------------------------------------------------
+## Syncronizing
+## ----------------------------------------------------------
+
+
+## ----------------------------------------------------------
+## Resources
+## ----------------------------------------------------------
+
+
+
+## ----------------------------------------------------------
+## Simple MapReduce
+## ----------------------------------------------------------
+
+class Generator( multiprocessing.Process ):
+    
+    def __init__( self, samples, s, e, result_queue ):
+        multiprocessing.Process.__init__(self)
+        self._samples = samples
+        self._int_start = s
+        self._int_end = e
+        self._result_queue = result_queue
+        self._run = True
+        print("[+] Created generator %d - %d with %d samples" % ( self._int_start, self._int_end, self._samples ) )
+
+    def run( self ):
+        proc_name = self.name
+        print("[+] %s Starting generator execution %s - %s with %s samples" % ( proc_name, self._int_start, self._int_end, self._samples ) )
+        for i in range( self._samples ):
+            self._result_queue.put( randint( self._int_end ) )
+    
+        return
+
+class Mapper( multiprocessing.Process ):
+
+    def __init__( self, task_queue, result_queue ):
+        multiprocessing.Process.__init__(self)
+        self._task_queue = task_queue
+        self._result_queue = result_queue
+        self._run = True
+
+    def run( self ):
+        pass
+
+
+class Reducer( multiprocessing.Process ):
+
+    def __init__( self, task_queue, result_queue ):
+        multiprocessing.Process.__init__(self)
+        self._task_queue = task_queue
+        self._result_queue = result_queue
+        self._run = True
+
+    def run( self ):
+        pass
+
+class Manager( multiprocessing.Process ):
+
+    def __init__( self, map_in_queue, red_in_queue, result_queue ):
+        multiprocessing.Process.__init__(self)
+        self._map_in_queue = map_in_queue
+        self._reduce_in_queue = red_in_queue
+        self._result_queue = result_queue
+        self._run = True
+
+    def run( self ):
+        pass
+    
+
+def mapreducer():
+    t_size = 100000
+    t_interval = (0, 256)
+    n_map_procs = randint( 5 )
+    n_red_procs = randint( 5 )
+
+    multiprocessing.set_start_method('fork')
+
+    print("[ ] Creating %d map %d reduce processes ..." % ( n_map_procs, n_red_procs ) )
+    map_tasks = multiprocessing.Queue( )
+    red_tasks = multiprocessing.Queue( )
+    map_results = multiprocessing.Queue( )
+    red_results = multiprocessing.Queue( )
+
+    print("[ ] Generating  %d samples in interval %s - %s ..." % ( t_size, t_interval[0], t_interval[1] ) )
+    generator = Generator( t_size, t_interval[0], t_interval[1], map_tasks )
+    generator.start()
+
+
+
+
+
+## ----------------------------------------------------------
+## OOP version
+## ----------------------------------------------------------
 class Consumer( multiprocessing.Process ):
     def __init__( self , task_queue, result_queue ):
         multiprocessing.Process.__init__(self)
@@ -52,10 +169,15 @@ class Task( object ):
 
 
 
-
-
+## ----------------------------------------------------------
+## Utils
+## ----------------------------------------------------------
 def randint( rng = 10 ):
     return random.randrange( rng )
+
+## ----------------------------------------------------------
+## Simple function based
+## ----------------------------------------------------------
 
 
 def simple_worker( i, t ):
@@ -116,9 +238,14 @@ def run_oo_version( ):
         print('Result:', result )
         jobs -= 1
 
+
+## ----------------------------------------------------------
+##  MAIN
+## ----------------------------------------------------------
+
 if __name__ == "__main__":
 
     opt = dict()
 #    run_simple()
-
-    run_oo_version()
+#    run_oo_version()
+    mapreducer()
